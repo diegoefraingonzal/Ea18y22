957574c1813dc3ffe2d1e6dd76aa4fa026665385
diegoefraingonzal <13064@portalcedi.edu.mx>
2025-04-08 10:42:48 -0600

    Plugin Geolocalization

--- sumativas/src/app/home/home.page.ts
+++ sumativas/src/app/home/home.page.ts
@@ -1,12 +1,36 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import {
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonContent,
+  IonButton,
+
+} from '@ionic/angular/standalone';
+import { Geolocation } from '@capacitor/geolocation';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  standalone: true,
+  imports: [ IonHeader, IonToolbar, IonTitle, IonContent, IonButton],
 })
 export class HomePage {
+  latitude: number | null = null;
+  longitude: number | null = null;
+
   constructor() {}
+
+  getCurrentPosition = async () => {
+    try {
+      const position = await Geolocation.getCurrentPosition();
+      this.latitude = position.coords.latitude;
+      this.longitude = position.coords.longitude;
+      console.log('Latitud:', this.latitude);
+      console.log('Longitud:', this.longitude);
+    } catch (error) {
+      console.error('Error obteniendo la ubicación', error);
+    }
+  };
 }
